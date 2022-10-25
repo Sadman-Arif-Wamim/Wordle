b401bb60ddb75e08da42901967b675cb730efc93
Sadman Arif Wamim <sadmanarifwamim11@gmail.com>
2022-10-25 15:22:02 +0600

    Implemented attempt loop

--- play_wordle.py
+++ play_wordle.py
@@ -3,13 +3,15 @@ from wordle import Wordle
 def main():
     print("Hello Wordle!")
     wordle = Wordle("apple")
-    print(wordle)
-    while True:
+
+    while wordle.can_attempt:
         x = input("Type your guess: ")
-        if x == wordle.secret:
-            print("You have guessed the word!")
-            break
-        print("Your guess is incorrect!")
+        wordle.attempts.append(x)
+       
+    if wordle.is_solved:
+        print("You've solved the puzzle.")
+    else:
+        print("You failed to solve!")
 
 if __name__ == "__main__":
     main()
--- wordle.py
+++ wordle.py
@@ -8,14 +8,18 @@ class Wordle:
         self.attempts = []
         pass
 
+    def attempt(self, word: str):
+        self.attempts.append(word)
+
+
     @property
     def is_solved(self):
-        return self.attempts[-1] == self.secret
+        return len(self.attempts) > 0 and self.attempts[-1] == self.secret
 
     @property
     def remaining_attempts(self) -> int:
-        self.Max_Attempts - len(self.attempts)
+        return self.Max_Attempts - len(self.attempts)
 
     @property
     def can_attempt(self):
-        return self.remaining_attempts == 0 and self.is_solved
+        return self.remaining_attempts > 0 and not self.is_solved
